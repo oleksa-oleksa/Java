75213bec56600d289dd493a5087a92b6cb4f6e0c
Oleksandra Baga <oleksandra.baga@gmail.com>
2017-07-01 16:46:27 +0200

    folder reorganisation

--- Informatik3/java.project/IN3/src/in3/a10/Command.java
+++
@@ -1,8 +0,0 @@
-package in3.a10;
-
-public class Command {
-
-	public static final String NEXT_PHASE = "next_phase";
-	public static final String STOP_ALL = "stop_all";
-	public static final String RESET_ALL = "reset_all";
-}
--- Informatik3/java.project/IN3/src/in3/a11/Ampelphase.java
+++
@@ -1,9 +0,0 @@
-package in3.a11;
-
-public class Ampelphase {
-	public static final String RED = "rot";
-	public static final String YELLOW = "gelb";
-	public static final String GREEN = "gruen";
-	public static final String RED_YELLOW = "rot-gelb";
-
-}
