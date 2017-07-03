5d2287d8245eaf72733b2b8a851d5ed71978a282
Oleksandra Baga <oleksandra.baga@gmail.com>
2017-07-04 00:22:12 +0200

    EventListener und Swing GUI Task done

--- Java_ThirdSemester_UniTasks/IN3/src/in3/a13/LightsGridLayoutButtons.java
+++ Java_ThirdSemester_UniTasks/IN3/src/in3/a13/LightsGridLayoutButtons.java
@@ -1,5 +1,7 @@
 package in3.a13;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
@@ -15,16 +17,22 @@ public class LightsGridLayoutButtons extends JPanel{
 		setLayout(new GridLayout(LINE_MAX, 0));
 
 		JButton b1 = new JButton("Next");
+		b1.setForeground(Color.BLUE);
+		b1.setFont((new Font ("Verdana", 50, 16)));
 		b1.addActionListener(as);
 		b1.setActionCommand(Command.NEXT_PHASE);
 		add(b1);
 		
 		JButton b2 = new JButton("Stop");
+		b2.setForeground(Color.RED);
+		b2.setFont((new Font ("Verdana", 50, 16)));
 		b2.addActionListener(as);
 		b2.setActionCommand(Command.STOP_ALL);
 		add(b2);
 		
 		JButton b3 = new JButton("Reset");
+		b3.setForeground(Color.ORANGE);
+		b3.setFont((new Font ("Verdana", 50, 16)));
 		b3.addActionListener(as);
 		b3.setActionCommand(Command.RESET_ALL);
 		add(b3);
